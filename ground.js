4ed285cdd7bf495ec9eba4037b26bbbf88ac8b27
adithikini <74593532+adithikini@users.noreply.github.com>
2020-12-20 12:57:07 +0530

    Add files via upload

---
+++ ground.js
@@ -0,0 +1,21 @@
+class Ground {
+    constructor(x,y,width,height){
+        var ground_options={
+            isStatic : true
+        }
+       this.ground=Bodies.rectangle(x,y,width,height,ground_options)
+       World.add(world,this.ground)
+       this.width=width;
+       this.height=this.height;
+
+       
+    }
+   display(){
+    fill("black");
+    rectMode(CENTER)
+    rect(this.ground.position.x,this.ground.position.y,this.width,this.height)
+    
+       
+   }
+
+}
